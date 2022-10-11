dbc6e7a0f3546f417a971fde6a4b50eea22a6d6d
SagarRajwar1603 <sagarraj16.sr@gmail.com>
2022-10-11 20:30:23 +0530

    Test1

---
+++ Introduction-to-JS-Variables-and-Data-Types/print04.js
@@ -0,0 +1,79 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+let college_name = "..........𝔹.𝔹.𝕄 ℂ𝕆𝕃𝕃𝔼𝔾𝔼, 𝔹𝔸𝕃𝕀𝔸ℙ𝕌ℝ...........";
+console.log(college_name);
+let location = ".................𝔻ℍ𝔸ℕ𝔹𝔸𝔻....................";
+console.log(location);
+
+
+ 
+console.log("*");
+
+
+let Report_card = ".................🆁🅴🅿🅾🆁🆃 🅲🅰🆁🅳................";
+console.log(Report_card);
+
+
+console.log("*");
+
+
+let Student_name = ".S̳t̳u̳d̳e̳n̳t̳ N̳a̳m̳e̳  : Sagar Rajwar";
+console.log(Student_name);
+let Rollno = ".R̳o̳l̳l̳ n̳o̳.̳      : fw22_0147";
+console.log(Rollno);
+let Class = ".C̳l̳a̳s̳s̳         : 10th";
+console.log(Class);
+let section = ".S̳e̳c̳t̳i̳o̳n̳       : B";
+console.log(section);
+
+ 
+console.log("*");
+
+
+let subject_section = "..................S̲u̲b̲j̲e̲c̲t̲s̲..................";
+console.log(subject_section);
+
+ 
+console.log("*");
+
+
+let subject = ".........E̳n̳g̳l̳i̳s̳h̳........S̳c̳i̳e̳n̳c̳e̳........M̳a̳t̳h̳s̳";
+console.log(subject);
+ 
+console.log("*");
+
+
+let Marks = ".Marks       80            72         78";
+console.log(Marks);
+let Grade =".Grade       A2            B1         B1";
+console.log(Grade);
+
+
+console.log("*");
+
+
+let result = ".R̳e̳s̳u̳l̳t̳   :  PASS";
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
